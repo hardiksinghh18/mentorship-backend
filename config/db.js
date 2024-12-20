922726d49eb7257444f88e9fd681496307ddcda3
Hardik Singh <hardiksingh950@gmail.com>
2024-12-20 19:40:08 +0530

    fix

--- config/db.js
+++ config/db.js
@@ -7,8 +7,14 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD, 
   {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT ||'mysql', // Or 'postgres' for postgres database
+    dialect: process.env.DB_DIALECT || 'mysql', // Or 'postgres' for PostgreSQL
     dialectModule: require('mysql2'),
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 );
 
